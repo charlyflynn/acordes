b096b5830041cc0751f723a4319863b9ac2cf4f1
Charlie Flynn <charlie.flynn@ibm.com>
2021-02-03 18:14:47 +0000

    refactor

--- server/index.js
+++ server/index.js
@@ -4,11 +4,11 @@ const app = express(); // create express app
 
 // add middlewares
 app.use(express.static(path.join(__dirname, "../react-app", "build")));
-app.use(express.static("../react-app/public"));
+// app.use(express.static("../react-app/public"));
 
-app.use((req, res, next) => {
-  res.sendFile(path.join(__dirname, "../react-app", "build", "index.html"));
-});
+// app.use((req, res, next) => {
+//   res.sendFile(path.join(__dirname, "../react-app", "build", "index.html"));
+// });
 
 // start express server on port 5000
 app.listen(5000, () => {
